25d5234ca5682c04803fc8932bb9c3a54627659c
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-03 22:22:26 +0100

    created class, adding attributes and methods

--- src/com/emilkowalczyk/Logic/Book.java
+++ src/com/emilkowalczyk/Logic/Book.java
@@ -1,2 +1,46 @@
-package com.emilkowalczyk.Logic;public class Book {
+package com.emilkowalczyk.Logic;
+
+public class Book {
+    private String title;
+    private Integer yearOfPublic;
+    private Integer id;
+
+    public Book(String title, Integer yearOfPublic, Integer id) {
+        this.title = title;
+        this.yearOfPublic = yearOfPublic;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYearOfPublic() {
+        return yearOfPublic;
+    }
+
+    public void setYearOfPublic(Integer yearOfPublic) {
+        this.yearOfPublic = yearOfPublic;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", yearOfPublic=" + yearOfPublic +
+                ", id=" + id +
+                '}';
+    }
 }
