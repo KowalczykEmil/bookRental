9396722148fda03c376cf1310b7efc80625040e1
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-03 22:25:59 +0100

    created class

--- src/com/emilkowalczyk/Logic/DataBaseMenager.java
+++ src/com/emilkowalczyk/Logic/DataBaseMenager.java
@@ -1,2 +1,4 @@
-package com.emilkowalczyk.Logic;public class DataBaseMenager {
+package com.emilkowalczyk.Logic;
+
+public class DataBaseMenager {
 }
--- src/com/emilkowalczyk/Logic/SQLDataService.java
+++ src/com/emilkowalczyk/Logic/SQLDataService.java
@@ -1,2 +1,4 @@
-package com.emilkowalczyk.Logic;public class SQLDataService {
+package com.emilkowalczyk.Logic;
+
+public class SQLDataService {
 }
