e6858dfcffb694622dab8beec99de50ee5cd28e5
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-03 22:21:43 +0100

    created class, adding attributes and methods

---
+++ src/com/emilkowalczyk/Logic/Author.java
@@ -0,0 +1,48 @@
+package com.emilkowalczyk.Logic;
+
+public class Author {
+    private String name;
+    private String lastName;
+    private Integer ID;
+
+
+    public Author(String name, String lastName, Integer ID) {
+        this.name = name;
+        this.lastName = lastName;
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", ID=" + ID +
+                '}';
+    }
+
+}
---
+++ src/com/emilkowalczyk/Logic/Book.java
@@ -0,0 +1,2 @@
+package com.emilkowalczyk.Logic;public class Book {
+}
