31d511364a9dfe426b5f2ec74ebf53425581fab6
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-03 22:29:36 +0100

    testing on a created person

--- src/com/emilkowalczyk/Main.java
+++ src/com/emilkowalczyk/Main.java
@@ -1,8 +1,12 @@
 package com.emilkowalczyk;
 
+import com.emilkowalczyk.Logic.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("EloELo");
-}
+        Person person = new Person("Emil","Kowalczyk", 11, 8,1999, "Pomocy@gmail.com",9901515);
+
+        System.out.println(person.toString());
+    }
 }
