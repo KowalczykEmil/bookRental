189097b6f5fac2a2851b7a3cf402885ead6159de
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-03 22:22:59 +0100

    created class, adding attributes and methods

--- src/com/emilkowalczyk/Logic/Person.java
+++ src/com/emilkowalczyk/Logic/Person.java
@@ -1,2 +1,39 @@
-package com.emilkowalczyk.Logic;public class Person {
+package com.emilkowalczyk.Logic;
+
+import java.util.ArrayList;
+
+public class Person {
+    private String name;
+    private String lastName;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
+    private String email;
+    private Integer phoneNumber;
+
+    public ArrayList<Book> books = new ArrayList<>();
+
+    public Person(String name, String lastName, int dayOfBirth, int monthOfBirth, int yearOfBirth, String email, Integer phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastName + '\'' +
+                ", dayOfBirth=" + dayOfBirth +
+                ", monthOfBirth=" + monthOfBirth +
+                ", yearOfBirth=" + yearOfBirth +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
