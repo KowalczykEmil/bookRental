163bbd4fa0d656ef6e99be3e6827f77462841926
KowalczykEmil <pomocyakoshi@gmail.com>
2021-01-27 14:16:19 +0100

    added method to automatically connect to the database server

--- src/com/emilkowalczyk/Main.java
+++ src/com/emilkowalczyk/Main.java
@@ -1,8 +1,13 @@
 package com.emilkowalczyk;
 
+import com.emilkowalczyk.Logic.DataBaseMenager;
 import com.emilkowalczyk.Logic.Interface;
 import com.emilkowalczyk.Logic.Person;
 import com.emilkowalczyk.Logic.PersonManager;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.IOException;
+import java.sql.Connection;
 
 public class Main {
 
@@ -10,6 +15,17 @@ public class Main {
         // Person Emil = new Person("Emil","Kowalczyk", 11, 8,1999, "Pomocy@gmail.com","9901515");
         // rsonManager personManager = new PersonManager();
         // personManager.addNewPerson(Emil);
+
+        String command = "E:\\Book_Rental\\mysql\\mysql-8.0.23-winx64\\bin\\mysqld.exe";
+        try
+        {
+            Process process = Runtime.getRuntime().exec(command);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
 
         Interface i = new Interface();
         i.showMenu();
